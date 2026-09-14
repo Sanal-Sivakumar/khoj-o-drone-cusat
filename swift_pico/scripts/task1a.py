bdef4f6b73771aa65fd4a9ecc638f33049be07a1
jforjoe <joelsiby2003@gmail.com>
2026-09-15 03:48:16 +0530

    Task1 Release

---
+++ swift_pico/scripts/task1a.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+# This script is used to run the first task 1A of Khojo Drone.
+
+# You will be building the image processing pipeline for the drone. The pipeline will take in an image and output a txt file. 
